e1cbd71b9dfc11e49f5768893c3d97c978393be9
bubbobne <daniele.andreis@gmail.com>
2026-04-09 15:57:54 +0200

    clean

---
+++ src/main/java/org/geoframe/snowmelting/deprecated/snowmeltingrastercase/EnergyIndex.java
@@ -0,0 +1,10 @@
+package org.geoframe.snowmelting.deprecated.snowmeltingrastercase;
+
+/**
+ * The  Model interface.
+ */
+public interface EnergyIndex {
+
+	abstract public double eiValues ();
+
+}
